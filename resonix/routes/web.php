dd8bb26aa543ead65b66def4ca63cfc3f9928fc2
Bhavish <bhavish.muneshwar@rasonix.com>
2023-05-05 11:10:57 +0530

    middleware for admin added

--- resonix/routes/web.php
+++ resonix/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,6 +39,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('admin')->group(function (){
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function (){
      Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class,'index']);
 });
