50ec549589e6d8c9375ac7576407d718019dffdf
MharanurNitin <nitinmharanur@gmail.com>
2023-05-07 08:07:04 +0530

    update user role completed

--- resonix/routes/web.php
+++ resonix/routes/web.php
@@ -57,4 +57,8 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('/post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'edit']);
     Route::put('update-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'update']);
     Route::get('delete-post/{post_id}', [App\Http\Controllers\Admin\PostController::class, 'destroy']);
+
+    Route::get('users', [App\Http\Controllers\Admin\UserController::class, 'index']);
+    Route::get('user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'edit']);
+    Route::put('update-user/{user_id}', [App\Http\Controllers\Admin\UserController::class, 'update']);
 });
